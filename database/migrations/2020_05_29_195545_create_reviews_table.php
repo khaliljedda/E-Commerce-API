107d465e0035baa8893ed910f141636c0f904822
khaliljedda <khaliljedda4@gmail.com>
2020-05-30 01:53:32 +0200

    migration

--- database/migrations/2020_05_29_195545_create_reviews_table.php
+++ database/migrations/2020_05_29_195545_create_reviews_table.php
@@ -15,8 +15,13 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
+            $table->string('customer');
+            $table->string('review');
+            $table->integer('star');
             $table->timestamps();
-        });
+                });
     }
 
     /**
